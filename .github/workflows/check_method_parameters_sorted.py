e47f316fcab64785abff9258fe41992ad784f590
jorisschellekens <joris.schellekens.1989@gmail.com>
2025-06-19 00:31:40 +0200

    update check_method_parameters_sorted.py to new workflow template

--- .github/workflows/check_method_parameters_sorted.py
+++ .github/workflows/check_method_parameters_sorted.py
@@ -1,160 +1,216 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import ast
+import collections
 import pathlib
 import re
-import sys
 import typing
+import sys
 
-KNOWN_EXCEPTIONS: typing.List[str] = [
-    "cmyk_color.py::__init__",
-    "fixed_column_width_table.py::__init__",
-    "flexible_column_width_table.py::__init__",
-    "inside.py::__init__",
-    "map.py::scale_to_fit",
-    "page_content_stream_processor.py::_apply_c",
-    "page_content_stream_processor.py::_apply_d1",
-    "page_content_stream_processor.py::_apply_k",
-    "page_content_stream_processor.py::_apply_K",
-    "page_content_stream_processor.py::_apply_re",
-    "page_content_stream_processor.py::_apply_RG",
-    "page_content_stream_processor.py::_apply_rg",
-    "page_content_stream_processor.py::_apply_v",
-    "pdf_bytes.py::next_eof_keyword",
-    "pdf_bytes.py::next_integer",
-    "pdf_bytes.py::next_newline",
-    "pdf_bytes.py::next_space",
-    "pdf_bytes.py::next_startxref_keyword",
-    "pdf_bytes.py::next_start_of_dictionary",
-    "pdf_bytes.py::next_start_of_pdf_keyword",
-    "pdf_bytes.py::previous_eof_keyword",
-    "pdf_bytes.py::previous_newline",
-    "pdf_bytes.py::previous_startxref_keyword",
-    "pdf_bytes.py::previous_start_of_pdf_keyword",
-    "pdf_bytes.py::__find_next",
-    "pdf_bytes.py::__find_previous",
-    "primitives.py::__init__",
-    "reference_visitor.py::__look_up_reference",
-    "rgb_color.py::__init__",
-    "shape.py::scale_to_fit",
-    "source.py::image",
-    "source.py::text",
-    "table.py::__init__",
-    "text_event.py::__init__",
-    "true_type_font.py::__get_cmap_for_type_0_font",  # turns out this script can not deal with mypy type comments
-    "xref_visitor.py::_get_value_from_dictionary_bytes",
-]
+WarningType = collections.namedtuple(
+    "WarningType",
+    field_names=[
+        "file",
+        "function",
+        "line_number",
+        "text",
+    ],
+)
 
 
-def main(root: str):
-    stk_todo = [pathlib.Path(root)]
-    stk_done = []
-    while len(stk_todo) > 0:
-        f = stk_todo.pop(0)
-        if f.is_dir():
+class ArgNameChecker(ast.NodeVisitor):
 
-            # IF we have reached the /.github directory
-            # THEN do not delve into it
-            if f.name == ".github":
-                continue
+    #
+    # CONSTRUCTOR
+    #
 
-            # IF we have reached the /.venv directory
-            # THEN do not delve into it
-            if f.name == ".venv":
-                continue
+    def __init__(self):
+        self.__warnings: typing.List[WarningType] = []
 
-            # IF we have reached the /tests directory
-            # THEN do not delve into it
-            if f.name == "tests":
-                continue
+    #
+    # PUBLIC
+    #
 
-            stk_todo += [subdir for subdir in f.iterdir()]
-        else:
-            if f.name.endswith(".py"):
-                stk_done += [f]
+    def get_warnings(self) -> typing.List[WarningType]:
+        return self.__warnings
 
-    def __extract_method_name(s: str) -> str:
-        s = s[s.find("    def ") + 8 :]
-        s = s[: s.find("(")]
-        return s
+    def visit_FunctionDef(self, node):
+        # IF the function is private
+        # THEN ignore it
+        if node.name.startswith("__"):
+            return
+        if node.name.startswith("_"):
+            return
 
-    def __extract_method_parameters(s: str) -> typing.List[str]:
-        open_bracket_count: int = 0
-        args: typing.List[str] = [""]
-        for c in s:
-            if c == ",":
-                if open_bracket_count != 0:
-                    args[-1] += c
-                else:
-                    args += [""]
-                continue
-            if c == "[":
-                open_bracket_count += 1
-                args[-1] += c
-                continue
-            if c == "]":
-                open_bracket_count -= 1
-                args[-1] += c
-                continue
-            args[-1] += c
-        return args
+        # split arguments in groups
+        number_of_args: int = len(node.args.args)
+        number_of_args_with_defaults: int = len(node.args.defaults)
+        number_of_args_without_defaults: int = (
+            number_of_args - number_of_args_with_defaults
+        )
+        args_without_default = [
+            x
+            for i, x in enumerate(node.args.args)
+            if i < number_of_args_without_defaults
+        ]
+        args_with_default = [
+            x
+            for i, x in enumerate(node.args.args)
+            if i >= number_of_args_without_defaults
+        ]
+        self_or_class_arg = []
+        if len(args_without_default) > 0 and args_without_default[0].arg in [
+            "self",
+            "cls",
+        ]:
+            self_or_class_arg = [args_without_default[0]]
+            args_without_default = args_without_default[1:]
 
-    # loop over all .py files
-    function_def_regex: re.Pattern = re.compile(
-        "def (?P<name>[^()]+)\\((?P<args>[^()]+)\\)"
-    )
-    exit_code: int = 0
-    nof_bad_files: int = 0
-    for python_file in stk_done:
-        text: str = ""
-        with open(python_file, "r") as python_file_handle:
-            text = python_file_handle.read()
+        # sort arguments
+        sorted_args = []
+        sorted_args += sorted(self_or_class_arg)
+        sorted_args += sorted(args_without_default, key=lambda x: x.arg)
+        sorted_args += sorted(args_with_default, key=lambda x: x.arg)
 
-        # handle \n
-        while "\n" in text:
-            text = text.replace("\n", "")
+        for i in range(0, len(sorted_args)):
 
-        # find all function definitions
-        for m in re.findall(function_def_regex, text):
+            # IF the argument order does not match
+            # THEN generate a warning
+            if node.args.args[i] != sorted_args[i]:
+                self.__warnings += [
+                    WarningType(
+                        file="",
+                        function=node.name,
+                        line_number=node.lineno,
+                        text=f"Expected {sorted_args[i].arg}, found {node.args.args[i].arg}",
+                    )
+                ]
+        self.generic_visit(node)
 
-            # build args_with_default
-            args: typing.List[str] = [
-                x.strip() for x in __extract_method_parameters(m[1])
+
+class CheckSomethingTemplate:
+
+    #
+    # CONSTRUCTOR
+    #
+
+    def __init__(
+        self,
+        root: pathlib.Path,
+        known_exceptions: typing.List[typing.Union[pathlib.Path, str]],
+    ):
+        self.__root: pathlib.Path = root
+        self.__warnings: typing.List[WarningType] = []
+        self.__number_of_files_checked: int = 0
+        self.__known_exceptions: typing.List[pathlib.Path] = known_exceptions
+
+    #
+    # PRIVATE
+    #
+
+    #
+    # PUBLIC
+    #
+
+    def get_number_of_files_checked(self) -> int:
+        return self.__number_of_files_checked
+
+    def get_warnings(self) -> typing.List[WarningType]:
+        return self.__warnings
+
+    def perform_check_on_directory(self) -> None:
+        stk_todo = [self.__root]
+        stk_done = []
+        while len(stk_todo) > 0:
+            f = stk_todo.pop(0)
+            if f.is_dir():
+
+                # IF we have reached the /.github directory
+                # THEN do not delve into it
+                if f.name == ".github":
+                    continue
+
+                # IF we have reached the /.venv directory
+                # THEN do not delve into it
+                if f.name == ".venv":
+                    continue
+
+                # IF we have reached the /tests directory
+                # THEN do not delve into it
+                if f.name == "tests":
+                    continue
+                stk_todo += [subdir for subdir in f.iterdir()]
+            else:
+                if f.name.endswith(".py"):
+                    stk_done += [f]
+
+        # filter out known_exceptions
+        # fmt: off
+        known_exceptions_01 = [x for x in self.__known_exceptions if isinstance(x, pathlib.Path)]
+        known_exceptions_02 = [re.compile(x) for x in self.__known_exceptions if isinstance(x, str)]
+        stk_done = [x for x in stk_done if x not in known_exceptions_01]
+        stk_done = [x for x in stk_done if not any([y.match(x.name) is not None for y in known_exceptions_02])]
+        # fmt: on
+
+        # perform_check_on_file for each item in stk_done
+        stk_done.sort(key=lambda x: x.name)
+        for f in stk_done:
+            self.perform_check_on_file(f)
+
+    def perform_check_on_file(self, file: pathlib.Path) -> None:
+        self.__number_of_files_checked += 1
+
+        with open(file, "r", encoding="utf-8") as f:
+            tree = ast.parse(f.read(), filename=file)
+            arg_name_checker: ArgNameChecker = ArgNameChecker()
+            arg_name_checker.visit(tree)
+            self.__warnings += [
+                WarningType(
+                    file=file,
+                    function=x.function,
+                    line_number=x.line_number,
+                    text=x.text,
+                )
+                for x in arg_name_checker.get_warnings()
             ]
-            args = [x for x in args if len(x) > 0]
-            args_with_default: typing.List[str] = [x for x in args if "=" in x]
-
-            # build args_without_default
-            args_without_default: typing.List[str] = [x for x in args if "=" not in x]
-            if "self" in args:
-                args_without_default.remove("self")
-
-            # build sorted_args
-            sorted_args = []
-            if "self" in args:
-                sorted_args += ["self"]
-            sorted_args += sorted(args_without_default)
-            sorted_args += sorted(args_with_default)
-
-            identifier: str = f"{python_file.name}::{m[0]}"
-            if identifier in KNOWN_EXCEPTIONS:
-                continue
-
-            # print
-            if args != sorted_args:
-                exit_code = 1
-                nof_bad_files += 1
-                print(f"Methods arguments unsorted in {identifier}")
-                print(f"\t- <ACTUAL> \t <EXPECTED>")
-                for a0, a1 in zip(args, sorted_args):
-                    print(f"\t- {a0} \t {a1}")
-
-    # print
-    if nof_bad_files != 0:
-        print(f"Found {nof_bad_files} METHODS with unsorted parameters.")
-
-    # exit
-    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    checker = CheckSomethingTemplate(
+        root=pathlib.Path(sys.argv[1]),
+        known_exceptions=["pdf_bytes.py", "source.py"],
+    )
+    checker.perform_check_on_directory()
+    warnings: typing.List[WarningType] = checker.get_warnings()
+
+    # IF everything passed
+    # THEN display pass text
+    if len(warnings) == 0:
+        print(
+            f"Finished checking {checker.get_number_of_files_checked()} file(s), everything OK"
+        )
+
+    # IF there are warnings
+    # THEN display them
+    if len(warnings) > 0:
+        print(
+            f"Finished checking {checker.get_number_of_files_checked()} file(s), found {len(warnings)} warnings:"
+        )
+        for w in warnings:
+            if (
+                w.file is not None
+                and w.function is not None
+                and w.line_number is not None
+                and w.text is not None
+            ):
+                print(f"{w.file}:{w.function}:{w.line_number} {w.text}")
+                continue
+            if w.file is not None and w.line_number is not None and w.text is not None:
+                print(f"{w.file}:{w.line_number} {w.text}")
+                continue
+            if w.file is not None and w.text is not None:
+                print(f"{w.file} {w.text}")
+                continue
+            if w.file is not None:
+                print(f"{w.file}")
+                continue
+
+        sys.exit(1)
